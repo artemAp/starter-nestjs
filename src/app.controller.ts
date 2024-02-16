2766b14fee6fef5b349ca20735ed9938d2640327
Artem Apukhtin <artem.apukhtin@globalpay.com>
2024-02-16 10:06:34 -0700

    add realip lib

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Headers, Ip, Req } from '@nestjs/common';
+import { Controller, Get, Headers, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RealIP } from 'nestjs-real-ip';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  async getHello(@Ip() ip: string, @Req() req: any, @Headers() headers: any): Promise<string> {
+  async getHello(@RealIP() ip: string, @Req() req: any, @Headers() headers: any): Promise<string> {
     console.log('IP', req.ip)
     console.log('connection', req.connection.remoteAddress)
     console.log('headers', headers.host)
