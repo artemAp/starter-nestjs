efdce9ea252e23adad7c60dfad0c23ce4388eef9
Artem Apukhtin <artem.apukhtin@globalpay.com>
2024-02-16 12:02:37 -0700

    Test final solituin

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,11 @@
-import { Controller, Get, Headers, Req } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common';
 import { RealIP } from 'nestjs-real-ip';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
 
   @Get()
-  async getHello(@RealIP() ip: string, @Req() req: any, @Headers() headers: any): Promise<string> {
-    console.log('IP', req.ip)
-    console.log('connection', req.connection.remoteAddress)
-    console.log('headers', headers.host)
-    return `${ip} ${req.headers['x-forwarded-for']}`;
+  async getHello(@RealIP() ip: string): Promise<string> {
+    return `Your IP Address is ${ip}`;
   }
 }
